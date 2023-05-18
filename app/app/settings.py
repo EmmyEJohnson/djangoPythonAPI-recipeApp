0bc863fd9e672cf6a210558d8fe0dce73f522c46
Emily Johnson <emelizajo22@gmail.com>
2023-05-17 19:45:09 -0700

    wip

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b*n9#qbb6&_7ryxl286=u0646a0b5cck7_i&&ai08tft16&*nc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
